9d05f742ef90c869895704f0ec503d344a8ef903
Peyton Cysewski <pcysewski12@gmail.com>
2020-09-17 17:24:25 -0700

    started writing code

---
+++ src/main/java/Bitmap.java
@@ -0,0 +1,13 @@
+import java.awt.image.BufferedImage;
+
+public class Bitmap {
+    public BufferedImage image;
+    public Bitmap(BufferedImage input) {
+        this.image = input;
+    }
+
+
+
+
+
+}
